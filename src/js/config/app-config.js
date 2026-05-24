f6840c31c3d1e3af500f45a22e66bade541182e9
Simon Kurtz <simonkurtz@gmail.com>
2026-05-24 16:42:55 -0400

    Set 2.0.0-beta.2

--- src/js/config/app-config.js
+++ src/js/config/app-config.js
@@ -4,7 +4,7 @@
 (function initializeAppConfig(globalScope) {
   const YEAR = 2026;
   const OFFICIAL_SOURCE_CHECKED_ON = '2026-05-24';
-  const APP_VERSION = '2.0.0-beta.1';
+  const APP_VERSION = '2.0.0-beta.2';
   const APP_LAST_UPDATED_ON = '2026-05-24';
 
   if (Object.prototype.hasOwnProperty.call(globalScope, 'YEAR')) {
