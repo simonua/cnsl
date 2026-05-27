2c5c28eaa3d22756585bd7b2644dbf6e0fb5d363
Simon Kurtz <simonkurtz@gmail.com>
2026-05-27 10:47:31 -0400

    Update What's New - Set 2.3.0

--- src/js/config/app-config.js
+++ src/js/config/app-config.js
@@ -5,7 +5,7 @@
   // Published site and active season metadata.
   const YEAR = 2026;
   const OFFICIAL_SOURCE_CHECKED_ON = '2026-05-27';
-  const APP_VERSION = '2.2.4';
+  const APP_VERSION = '2.3.0';
   const APP_LAST_UPDATED_ON = '2026-05-27';
   const HOME_PAGE_HOSTNAME = 'pools.longreachmarlins.org';
   const HOME_PAGE_URL = `https://${HOME_PAGE_HOSTNAME}`;
